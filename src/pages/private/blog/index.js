1344648c4e90fd747ff68b3fa0c6b612153ed14a
gaurav-nickelfox <gaurav.panday@nickelfox.com>
2022-09-07 16:44:08 +0530

    style(blogViewStyle):added styling for displaying the blog lists

--- src/pages/private/blog/index.js
+++ src/pages/private/blog/index.js
@@ -1,42 +1,44 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-// import fb from './firebase'
 import { fireBaseConnectionInstance } from "helpers";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 
 // const Blogs = db.collection('blogs');
 
- function Bloglist() {
+function Bloglist() {
   const [blogslist, setblogs] = useState([]);
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
+  const { db } = fireBaseConnectionInstance();
 
-//  const getBlogsCollectionData = async()=>{
-//  const { db } = fireBaseConnectionInstance();
-//  const querySnapshot = await getDocs(collection(db, "blogs"));
-//   querySnapshot.forEach((doc) => {
-//     arrPost.push({...doc.data(),key:doc.id})
-//  });
-//   }
-  useEffect(()=>{
-    const arrPost = []
-    const getBlogsCollectionData = async()=>{
-        const { db } = fireBaseConnectionInstance();
-        const querySnapshot = await getDocs(collection(db, "blogs"));
-         querySnapshot.forEach((doc) => {
-           arrPost.push({...doc.data(),id:doc.id})
-        });
-        setblogs(arrPost)
-        setLoading(false);
-         }
-        getBlogsCollectionData();
-      return ()=> getBlogsCollectionData()
-  },[])
-
+  //  const getBlogsCollectionData = async()=>{
+  //  const { db } = fireBaseConnectionInstance();
+  //  const querySnapshot = await getDocs(collection(db, "blogs"));
+  //   querySnapshot.forEach((doc) => {
+  //     arrPost.push({...doc.data(),key:doc.id})
+  //  });
+  //   }
+  useEffect(() => {
+    const arrPost = [];
+    const getBlogsCollectionData = async () => {
+      const querySnapshot = await getDocs(collection(db, "blogs"));
+      querySnapshot.forEach((doc) => {
+        arrPost.push({ ...doc.data(), id: doc.id });
+      });
+      setblogs(arrPost);
+      setLoading(false);
+    };
+    getBlogsCollectionData();
+    return () => getBlogsCollectionData();
+  }, []);
 
   const DeleteBlog = (id) => {
-    Blogs.doc(id)
-      .delete()
+    const docRef = doc(db, "blogs", id);
+    deleteDoc(docRef)
       .then(() => {
+        const updatedBlogList = blogslist.filter((list) => list.id !== id);
+        setblogs(updatedBlogList);
         alert("Document successfully deleted!");
       })
       .catch((error) => {
@@ -44,52 +46,62 @@ import { collection, getDocs } from "firebase/firestore";
       });
   };
 
-//   useEffect(() => {
-//     // Subscribe to query with onSnapshot
-//     const unsubscribe = Blogs.limit(100).onSnapshot((querySnapshot) => {
-//       // Get all documents from collection - with IDs
-//       const data = querySnapshot.docs.map((doc) => ({
-//         ...doc.data(),
-//         id: doc.id
-//       }));
-//       console.log({data});
-//       // Update state
-//       setblogs(data);
-//     });
+  //   useEffect(() => {
+  //     // Subscribe to query with onSnapshot
+  //     const unsubscribe = Blogs.limit(100).onSnapshot((querySnapshot) => {
+  //       // Get all documents from collection - with IDs
+  //       const data = querySnapshot.docs.map((doc) => ({
+  //         ...doc.data(),
+  //         id: doc.id
+  //       }));
+  //       console.log({data});
+  //       // Update state
+  //       setblogs(data);
+  //     });
 
-//     // Detach listener
-//     return unsubscribe;
-//   }, []);
- console.log(blogslist)
- if(loading){
-    return(<h1>Loading firebase data content......</h1>)
- }
+  //     // Detach listener
+  //     return unsubscribe;
+  //   }, []);
+  console.log(blogslist);
+  if (loading) {
+    return <h1>Loading firebase data content......</h1>;
+  }
   return (
     <div>
-      <h2 className="w-full text-center font-bold text-xl">All blogs List</h2>
+      <h2>All blogs List</h2>
       {blogslist.map((blog) => (
-        <div key={blog.id}>
-          <p>body: {blog.Body} </p>
-          <Link
-            to={"/blog/" + blog.id}
-            class="mr-2 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-1 px-2 border border-indigo-500 rounded">
-            View
-          </Link>
-          <Link
-            to={"/blog/edit/" + blog.id}
-            class="mr-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">
-            Edit
-          </Link>
-          <button
-            onClick={() => {
-              DeleteBlog(blog.id);
-            }}>
-            delete
-          </button>
-        </div>
+        <Card style={{ margin: "20px" }} variant="outlined" key={blog.id}>
+          <CardContent>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+              }}
+              id={blog.id}>
+              <div dangerouslySetInnerHTML={{ __html: blog.Body }}></div>
+              <div style={{ display: "flex", justifyContent: "space-between",minWidth:'15%' }}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    DeleteBlog(blog.id);
+                  }}>
+                  Edit
+                </Button>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    DeleteBlog(blog.id);
+                  }}>
+                  delete
+                </Button>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
 }
 
-export default Bloglist
+export default Bloglist;
